6259b283452b21dccf065bce081c5facc81734fb
Arian Rahimi <arianrahimi@icloud.com>
2019-09-26 23:19:40 +0000

    Fixed callback, added m&c

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,21 @@
 const request = require("request");
-let args = process.argv.slice(2);
 "use strict";
-//end of config code
+const fetchBreedDescription = function(breed, callback) {
+ request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, responseNotUsed, body) => {
 
+   if (error) {
+     callback(error,null);
+   }
 
-const breedFetch = function(breed) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+   const data = JSON.parse(body);
+   const breed = data[0];
 
-    if (error) {
-      console.log('ERR HERE!!!!: ', error);
-    }
+   if (breed) {
+     callback(null,breed.description);
+   } else {
+     callback(`${breed} isn't a breed!`,null);
+   }
 
-    const data = JSON.parse(body);
-    const breed = data[0];
-
-    if (breed) {
-      console.log(breed.description);
-    } else {
-      console.log('No such breed:', breed);
-    }
-    
-  });
-
+ });
 };
-
-breedFetch(args);
+module.exports = { fetchBreedDescription };
